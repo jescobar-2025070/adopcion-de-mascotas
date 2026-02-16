4fb67972a066262daa9b3bee5d38d2e9501bb8a0
tgutierrez-2022127 <tgutierrez-2022127@kinal.edu.gt>
2026-02-15 18:30:35 -0600

    feat: agregar estructura inicial del proyecto adopcionesapp

---
+++ adopcionesapp/src/main/java/com/vicionsa/adopcionesapp/AdopcionesappApplication.java
@@ -0,0 +1,13 @@
+package com.vicionsa.adopcionesapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdopcionesappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdopcionesappApplication.class, args);
+	}
+
+}
